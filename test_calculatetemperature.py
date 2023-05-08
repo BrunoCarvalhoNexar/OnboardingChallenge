fcca1b63b0947af59e6416518d7f3a44bbd82b8e
Nexar <nexar@Nexars-MBP.Home>
2023-05-08 20:42:56 +0100

    ident more

--- test_calculatetemperature.py
+++ test_calculatetemperature.py
@@ -3,6 +3,7 @@ from calculatetemperature import CalculateTemperature
 dictionary_temperature = {'January': 12, 'February': 13, 'March': 15, 'April': 16, 'May': 18, 'Juny': 21, 'July': 24,
                           'August': 25, 'September': 23, 'October': 19, 'November': 16, 'December': 14}
 dictionary_temperature_values = [dictionary_temperature[k] for k,v in dictionary_temperature.items()]
+
 def test_average_temperature():
     avg_temperature = CalculateTemperature.avg_temperature(dictionary_temperature,dictionary_temperature_values)
     assert avg_temperature == 18.0
